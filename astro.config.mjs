3e3ee53f42b4cd67cd686003254925a05bdfccb5
cjnbennett <conor.jn.bennett@gmail.com>
2025-04-10 14:50:19 +0800

    icons working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://botanicblue.club',
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	experimental: {
+		svg: true,
+	},
 });
